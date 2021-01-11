714933ca27a78c476f248896fa7de64a1e415720
bekiboo <julien.connault@gmail.com>
2021-01-11 22:16:38 +0300

    added Ranch page

--- js/script.js
+++ js/script.js
@@ -7,3 +7,12 @@ document.getElementById("lastUpdate").textContent =
 var today = new Date();
 var currentYear = today.getFullYear();  
 document.getElementById("currentYear").textContent = currentYear;
+
+for (let i = 0; i < depth; i+2) {
+  const element = array[i];
+  
+}
+
+function name(params) {
+  
+}
