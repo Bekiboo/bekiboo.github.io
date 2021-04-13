068d20eca9679f6dac5a5582bc3a3187d72f0344
bekiboo <julien.connault@gmail.com>
2021-04-13 16:02:17 +0300

    Rearranged all files and updated Hub

--- lessons/lesson3/js/script.js
+++
@@ -1,9 +0,0 @@
-// Dynamic Last Update
-var date = document.lastModified;
-document.getElementById("lastUpdate").textContent =
-  "Last Updated: " + date;
-
-// Dynamic Year
-var today = new Date();
-var currentYear = today.getFullYear();  
-document.getElementById("currentYear").textContent = currentYear;
