345d97b0df07163f7b8d4a4e03388cf19bda8597
Bekiboo <julien.connault@gmail.com>
2021-04-20 17:05:04 +0300

    Elevatus Beta Home Page video test

--- elevatus-project/elevatus-beta/js/app.js
+++ elevatus-project/elevatus-beta/js/app.js
@@ -3,3 +3,7 @@ function toggleMenu() {
   document.getElementById("hamburger").classList.toggle("open");
   document.getElementById("js-nav").classList.toggle("display-nav");
 }
+
+$('.button').click(function () {
+  $(this).parent().toggleClass('expand');     
+});
