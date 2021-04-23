08c0a7e84b2a64c23c52127727c4eacc4418aee5
Bekiboo <julien.connault@gmail.com>
2021-04-23 14:48:06 +0300

    wdd-330 Week 1

---
+++ wdd-330/app.js
@@ -0,0 +1,12 @@
+const numberOfWeeks = 1;
+
+let listOfWeek = document.getElementById("weekList");
+
+for (let i = 1; i <= numberOfWeeks; i++) {
+    let li = document.createElement('li');
+    let link = document.createElement('a');
+    link.setAttribute('href', `week${[i]}/index.html`);
+    link.textContent= `Week ${[i]}`;
+    li.appendChild(link);
+    listOfWeek.appendChild(li)
+}
