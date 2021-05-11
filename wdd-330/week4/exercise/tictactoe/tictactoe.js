aac555805db9064d6588ff330f7f27f9073bcb04
Bekiboo <julien.connault@gmail.com>
2021-05-11 23:04:43 +0300

    tic tac toe bug fix (draw)

--- wdd-330/week4/exercise/tictactoe/tictactoe.js
+++ wdd-330/week4/exercise/tictactoe/tictactoe.js
@@ -1,15 +1,20 @@
-const boxAll = document.querySelectorAll('.box')
+const tileAll = document.querySelectorAll('.tile')
 const board = document.querySelector('.board')
 const button = document.querySelector('button')
 const resultDiv = document.querySelector('.result')
 
-boxAll.forEach(box => {
-    box.addEventListener('click', toggleBox)
+// Event listeners
+tileAll.forEach(tile => {
+    tile.addEventListener('click', toggletile)
 });
 button.addEventListener('click', resetBoard)
 
+// Board Setup
 let xTurn = true
 let turnNumber = 0
+
+const currentStateX = [] // which tiles have Xs
+const currentStateO = [] // which tiles have Os
 
 const winningCombo = [ // all possible winning combinations
     [1, 2, 3],
@@ -22,30 +27,30 @@ const winningCombo = [ // all possible winning combinations
     [3, 5, 7],
 ];
 
-const currentStateX = [] // which boxes have Xs
-const currentStateO = [] // which boxes have Os
-
-function toggleBox() {
-    boxState = this.dataset['state']
-    boxId = this.dataset['id']
+function toggletile() {
+    tileState = this.dataset['state']
+    tileId = this.dataset['id']
     turnNumber += 1
-    if (boxState == "" && turnNumber <= 9) {
+    if (tileState == "" && turnNumber <= 9) {
         if (xTurn == true) {
             this.setAttribute('data-state', 'x')
             board.setAttribute('data-turn', 'o') // sets board turn to "o"
             xTurn = false
-            currentStateX.push(parseInt(boxId))
-            if (checkWin(winningCombo, currentStateX)) endGame("X wins")
+            currentStateX.push(parseInt(tileId))
+            checkWin(winningCombo, currentStateX) ? endGame("X wins") : checkDraw(turnNumber)
 
         } else {
             this.setAttribute('data-state', 'o')
             board.setAttribute('data-turn', 'x') // sets board turn to "x"
             xTurn = true
-            currentStateO.push(parseInt(boxId))
-            if (checkWin(winningCombo, currentStateO)) endGame("O wins")
+            currentStateO.push(parseInt(tileId))
+            checkWin(winningCombo, currentStateO) ? endGame("O wins") : checkDraw(turnNumber)
         }
     }
-    if (turnNumber == 9) endGame("Draw");
+}
+
+function checkDraw(turn) {
+    if (turn == 9) endGame("Draw")    
 }
 
 function checkWin(solutions, state) {
@@ -56,17 +61,17 @@ function checkWin(solutions, state) {
 
 function endGame(finalState) {
     resultDiv.innerHTML = finalState
-    boxAll.forEach(box => { // prevents the player from continuing playing
-        if (box.dataset['state'] == '') {
-            box.setAttribute('data-state', 'end')
+    tileAll.forEach(tile => { // prevents the player from continuing playing
+        if (tile.dataset['state'] == '') {
+            tile.setAttribute('data-state', 'end')
         }
     })
 }
 
 function resetBoard() { // resets everything
     board.setAttribute('data-turn', 'x')
-    boxAll.forEach(box => {
-        box.setAttribute('data-state', '')
+    tileAll.forEach(tile => {
+        tile.setAttribute('data-state', '')
     })
     xTurn = true
     turnNumber = 0
