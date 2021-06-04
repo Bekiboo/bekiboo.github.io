feccce8cd1ebb148af69611c06e08974c11f7eb8
Bekiboo <julien.connault@gmail.com>
2021-06-04 21:20:34 +0300

    Week7 WDD-330

--- wdd-330/week4/exercise/finalProject/app.js
+++ wdd-330/week4/exercise/finalProject/app.js
@@ -1,20 +1,8 @@
-fetch('http://127.0.0.1:5500/week4/exercise/finalProject/childrenList.json')
+fetch('https://superheroapi.com/api/10158280548896917')
     .then((response) => response.json())
     .then((listOfChild) => {
         console.log(listOfChild);
 
-        m = 0
-        f = 0
-        
-        for (let i = 0; i < listOfChild.length; i++) {
-            if (listOfChild[i].gender == 'M') {
-                m += 1                           
-            } else {
-                f += 1
-            }
-        }
-
-        console.log(m);
-        console.log(f);
+       
     });
 
