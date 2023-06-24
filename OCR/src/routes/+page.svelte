ff9136bbf760498a40334a8ee9f2a239ef28460b
Bekiboo <julien.connault@gmail.com>
2023-06-24 19:10:03 +0300

    OCR test

---
+++ OCR/src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	import Tesseract from 'tesseract.js';
+
+	let text = 'Loading...';
+
+	onMount(() => {
+		Tesseract.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png', 'eng', {
+			logger: (m) => console.log(m)
+		}).then(({ data: { text: recognizedText } }) => {
+			console.log(recognizedText);
+			text = recognizedText;
+		});
+	});
+</script>
+
+<div class="container">
+	<h1>OCR</h1>
+	<p>{text}</p>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
