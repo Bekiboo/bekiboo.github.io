c3550af58f3c8ff34de3d3f82cbdec6df65f9922
Bekiboo <julien.connault@gmail.com>
2023-06-26 15:41:42 +0300

    gh-pages

--- OCR/src/routes/+page.svelte
+++ OCR/src/routes/+page.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	// import { onMount } from 'svelte';
 
-	import Tesseract from 'tesseract.js';
+	// import Tesseract from 'tesseract.js';
 
 	let text = 'Loading...';
 
-	onMount(() => {
-		Tesseract.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png', 'eng', {
-			logger: (m) => console.log(m)
-		}).then(({ data: { text: recognizedText } }) => {
-			console.log(recognizedText);
-			text = recognizedText;
-		});
-	});
+	// onMount(() => {
+	// 	Tesseract.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png', 'eng', {
+	// 		logger: (m) => console.log(m)
+	// 	}).then(({ data: { text: recognizedText } }) => {
+	// 		console.log(recognizedText);
+	// 		text = recognizedText;
+	// 	});
+	// });
 </script>
 
 <div class="container">
